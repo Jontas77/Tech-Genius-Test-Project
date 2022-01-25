cf706aecd93c758d2e26744cb44fc1f54fe7ce8d
Jonathan Heugh <mrjbheugh@gmail.com>
2022-01-25 16:39:04 +0200

    Added authentication using jwt

--- server/server.js
+++ server/server.js
@@ -3,7 +3,16 @@ const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => res.send("Welcome"));
+
+// Routes //
+
+//login
+app.use("/auth", require("./auth/authRoutes"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started at port: ${PORT}`));
